913b1a1252e94f67535b51f87a1eed9560cb9b9b
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-04-28 17:14:00 +0300

    deployment to heroku

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,19 @@
 
-from flask import render_template,request,redirect,url_for
-from app import app
-from .request import get_movies, get_movie,search_movie
-from .models import reviews
-from .forms import ReviewForm
-Review = reviews.Review
+# from flask import render_template,request,redirect,url_for
+# from app import app
+# from ..requests import get_movies, get_movie,search_movie
+# from ..models import reviews
+# from .forms import ReviewForm
+# Review = reviews.Review
 
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_movies,get_movie,search_movie
+from .forms import ReviewForm
+from ..models import Review
 
 # Views
-
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -26,22 +30,12 @@ def index():
     search_movie = request.args.get('movie_query')
 
     if search_movie:
-        return redirect(url_for('search',movie_name=search_movie))
+        return redirect(url_for('.search',movie_name=search_movie))
     else:
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
-# @app.route('/movie/<int:id>')    
-# def movie(id):
 
-#     '''
-#     View movie page function that returns the movie details page and its data
-#     '''
-#     movie = get_movie(id)
-#     title = f'{movie.title}'
-
-#     return render_template('movie.html',title = title,movie = movie)
-
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
 
     '''
@@ -54,7 +48,7 @@ def movie(id):
     return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
 
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -65,7 +59,7 @@ def search(movie_name):
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
 
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
@@ -75,7 +69,7 @@ def new_review(id):
         review = form.review.data
         new_review = Review(movie.id,title,movie.poster,review)
         new_review.save_review()
-        return redirect(url_for('movie',id = movie.id ))
+        return redirect(url_for('.movie',id = movie.id ))
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
