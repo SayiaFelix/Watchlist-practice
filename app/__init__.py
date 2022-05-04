ac0246832f98830ad4daa8b9a4e06a8fa8f47d8f
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-04 16:59:51 +0300

    adding readme

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,19 @@
-# from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-# from flask_mail import Mail
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
-# photos = UploadSet('photos',IMAGES)
+photos = UploadSet('photos',IMAGES)
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-# mail = Mail()
+mail = Mail()
 
 def create_app(config_name):
 
@@ -30,7 +30,7 @@ def create_app(config_name):
     login_manager.init_app(app)
     
     # configure UploadSet
-    # configure_uploads(app,photos)
+    configure_uploads(app,photos)
 
     # Registering the blueprint
     from .main import main as main_blueprint
--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jaysafu@localhost/watchlist'
-    # UPLOADED_PHOTOS_DEST ='app/static/photos'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 #    #  email configurations
 #     MAIL_SERVER = 'smtp.googlemail.com'
