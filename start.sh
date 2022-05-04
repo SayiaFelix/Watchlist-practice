fbf714c39e8b35298ed8af7d61cf754297e2d7d8
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-04 19:32:38 +0300

    adding readme

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export MOVIE_API_KEY='df8accb1b7d390dfcf3683c9df8d6413'
 export SECRET_KEY='-+v,.q5yv&M9!&8MuKd8?7XmgDTm5J'
-# export MAIL_USERNAME='sayiafelix18@gmail.com'
-# export MAIL_PASSWORD='0706206335'
+export MAIL_USERNAME='sayiafelix18@gmail.com'
+export MAIL_PASSWORD='0706206335'
 
 
 python3.9 manage.py server
