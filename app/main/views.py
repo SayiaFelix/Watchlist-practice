ad7148b1c432a24d154c734b3a5b7b4fb06c6710
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-04 10:15:34 +0300

    adding file

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,12 @@
 
-# from flask import render_template,request,redirect,url_for
-# from app import app
-# from ..requests import get_movies, get_movie,search_movie
-# from ..models import reviews
-# from .forms import ReviewForm
-# Review = reviews.Review
 
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..requests import get_movies,get_movie,search_movie
-from .forms import ReviewForm
-from ..models import Review
+from .forms import ReviewForm,UpdateProfile
+from ..models import Review,User
 from flask_login import login_required
+from .. import db
 
 # Views
 @main.route('/')
@@ -78,4 +73,30 @@ def new_review(id):
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
 
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
 
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
